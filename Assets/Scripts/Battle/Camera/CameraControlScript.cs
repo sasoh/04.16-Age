c215b0be76e9996bd4a30b953bab00e773e1e3e6
Sasho <sasoh@users.noreply.github.com>
2016-04-03 20:21:17 +0300

    Added files via upload

---
+++ Assets/Scripts/Battle/Camera/CameraControlScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControlScript : MonoBehaviour
+{
+    public float MovementSpeed = 10.0f;
+
+    void Update()
+    {
+        InputData input = InputManagerScript.GetInput();
+        ProcessInput(input);
+    }
+
+    void ProcessInput(InputData input)
+    {
+        Vector3 position = transform.position;
+
+        position.x += input.Horizontal * MovementSpeed;
+        position.z += input.Vertical * MovementSpeed;
+
+        transform.localPosition = position;
+    }
+}
---
+++ Assets/Scripts/Input/InputManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+struct InputData
+{
+    public float Horizontal;
+    public float Vertical;
+}
+
+class InputManagerScript
+{
+    public static InputData GetInput()
+    {
+        InputData result;
+
+        result.Horizontal = Input.GetAxis("Horizontal");
+        result.Vertical = Input.GetAxis("Vertical");
+
+        return result;
+    }
+}
